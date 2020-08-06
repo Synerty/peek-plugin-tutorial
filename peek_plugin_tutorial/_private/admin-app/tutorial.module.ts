9b846bc9ebaa55c5c8dcae95c32d7485f352dfc3
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-08-06 11:28:59 +1000

    Added root admin page for Tutorial Plugin.

---
+++ peek_plugin_tutorial/_private/admin-app/tutorial.module.ts
@@ -0,0 +1,33 @@
+import {CommonModule} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
+
+
+// Import our components
+import {TutorialComponent} from "./tutorial.component";
+
+// Define the routes for this Angular module
+export const pluginRoutes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: TutorialComponent
+    }
+
+];
+
+// Define the module
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(pluginRoutes),
+        FormsModule
+    ],
+    exports: [],
+    providers: [],
+    declarations: [TutorialComponent]
+})
+export class TutorialModule {
+
+}
