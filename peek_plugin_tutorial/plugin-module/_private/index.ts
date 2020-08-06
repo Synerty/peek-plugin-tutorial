ae436385b67e719261a516f0d11c03c865882518
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-08-07 08:44:21 +1000

    Configured TypeScript Tuples.

--- peek_plugin_tutorial/plugin-module/_private/index.ts
+++ peek_plugin_tutorial/plugin-module/_private/index.ts
@@ -1,1 +1,5 @@
 export * from "./PluginNames";
+
+export {TutorialTuple} from "./tuples/TutorialTuple";
+export {StringIntTuple} from "./tuples/StringIntTuple";
+export {SettingPropertyTuple} from "./tuples/SettingPropertyTuple";
