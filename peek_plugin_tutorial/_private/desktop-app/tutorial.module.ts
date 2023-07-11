4fb3cee4ef6267a1b038e5dd65508a6cae31c560
Louis Lu <louis.lu@synerty.com>
2023-07-12 11:05:59 +1200

    PEEK-2186 fixed with typed routes

--- peek_plugin_tutorial/_private/desktop-app/tutorial.module.ts
+++ peek_plugin_tutorial/_private/desktop-app/tutorial.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { RouterModule, Route, Routes } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { NzIconModule } from "ng-zorro-antd/icon";
 import { HttpClientModule } from "@angular/common/http";
